0047d2781555cc3850309da801b11de63b3757e7
kuriboh <geovanmafra@proton.me>
2025-05-31 22:19:29 -0300

    Update configuration.nix

--- etc/nixos/configuration.nix
+++ etc/nixos/configuration.nix
@@ -105,8 +105,8 @@
 
     # Some programs need SUID wrappers, can be configured further or are
     # started in user sessions.
-    # programs.mtr.enable = true;
-    # programs.gnupg.agent = {
+    # mtr.enable = true;
+    # gnupg.agent = {
     #   enable = true;
     #   enableSSHSupport = true;
     # };
