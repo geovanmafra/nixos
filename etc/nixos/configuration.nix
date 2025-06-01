15a5c649edcf6064fc848a7613a0ac69d1273ba1
kuriboh <geovanmafra@proton.me>
2025-05-31 21:54:18 -0300

    Update configuration.nix

--- etc/nixos/configuration.nix
+++ etc/nixos/configuration.nix
@@ -56,30 +56,6 @@
   #   useXkbConfig = true; # use xkb.options in tty.
   };
 
-  # Enable the X11 windowing system.
-  # services.xserver.enable = true;
-
-
-  
-
-  # Configure keymap in X11
-  # services.xserver.xkb.layout = "us";
-  # services.xserver.xkb.options = "eurosign:e,caps:escape";
-
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-  # Enable sound.
-  # services.pulseaudio.enable = true;
-  # OR
-  services.pipewire = {
-    enable = true;
-    pulse.enable = true;
-  };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.libinput.enable = true;
-
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.user = {
     isNormalUser = true;
@@ -201,32 +177,66 @@
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
 
   # List services that you want to enable:
-  services.getty = {
-    autologinOnce = true;
-    autologinUser = "user";
-  };
-  services.openssh.enable = true; # Enable the OpenSSH daemon.
-  services.udisks2.enable = true; # Automounting support.
-  services.hypridle.enable = true;
-  services.gvfs.enable = true; # Nautilus outside of Gnome.
-  hardware.bluetooth.enable = true; # Bluetooth support.
-  services.tlp = {
-    enable = true;
-    settings = {
-      CPU_SCALING_GOVERNOR_ON_AC = "performance";
-      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+  services = {
+    # Enable the X11 windowing system.
+    # xserver.enable = true;
 
-      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
-      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+    # Configure keymap in X11
+    # xserver.xkb.layout = "us";
+    # xserver.xkb.options = "eurosign:e,caps:escape";
 
-      CPU_MIN_PERF_ON_AC = 0;
-      CPU_MAX_PERF_ON_AC = 100;
-      CPU_MIN_PERF_ON_BAT = 0;
-      CPU_MAX_PERF_ON_BAT = 20;
+    # Enable CUPS to print documents.
+    # printing.enable = true;
 
-    #Optional helps save long term battery health
-    START_CHARGE_THRESH_BAT0 = 40; # 40 and below it starts to charge
-    STOP_CHARGE_THRESH_BAT0 = 80; # 80 and above it stops charging
+    # Enable sound.
+    # pulseaudio.enable = true;
+    # OR
+    pipewire = {
+      enable = true;
+      pulse.enable = true;
+    };
+
+    # Enable touchpad support (enabled default in most desktopManager).
+    libinput.enable = true;
+
+    # Enable autologin into TTY.
+    getty = {
+      autologinOnce = true;
+      autologinUser = "user";
+    };
+
+    # Enable the OpenSSH daemon.
+    openssh.enable = true;
+
+    # Automounting support.
+    udisks2.enable = true;
+
+    # Enable idle daemon.
+    hypridle.enable = true;
+
+    # Nautilus outside of Gnome.
+    gvfs.enable = true;
+    hardware.bluetooth.enable = true; # Bluetooth support.
+
+    # Power management.
+    services.tlp = {
+      enable = true;
+      settings = {
+        CPU_SCALING_GOVERNOR_ON_AC = "performance";
+        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+
+        CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+        CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+
+        CPU_MIN_PERF_ON_AC = 0;
+        CPU_MAX_PERF_ON_AC = 100;
+        CPU_MIN_PERF_ON_BAT = 0;
+        CPU_MAX_PERF_ON_BAT = 20;
+
+        #Optional helps save long term battery health
+        START_CHARGE_THRESH_BAT0 = 40; # 40 and below it starts to charge
+        STOP_CHARGE_THRESH_BAT0 = 80; # 80 and above it stops charging
+      };
     };
   };
 
